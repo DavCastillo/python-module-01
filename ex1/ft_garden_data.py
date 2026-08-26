49fe23d77291863da0661bb94994f4bdf575ff1b
David Eduardo Castillo Granados <davcasti@c3r2s4.42urduliz.com>
2026-08-26 15:26:32 +0200

    python module 01: ex0 y ex1 funcionando

---
+++ ex1/ft_garden_data.py
@@ -0,0 +1,23 @@
+class Plant:
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def show(self):
+        # print(self.name + ":", self.height, "cm,", self.age, "days old")
+        print(f"{self.name}: {self.height}cm, {self.age} days old")
+
+
+def ft_garden_data():
+    plant1 = Plant("Rose", 25, 30)
+    plant2 = Plant("Sunflower", 80, 45)
+    plant3 = Plant("Cactus", 15, 120)
+    print("=== Garden Plant Registry ===")
+    plant1.show()
+    plant2.show()
+    plant3.show()
+
+
+if __name__ == "__main__":
+    ft_garden_data()
